a8f6df3753a933ffddbeeff27b9c31f92449f7b4
pcataldo <pcataldo@student.rcc.edu>
2015-09-25 15:59:17 -0700

    Inventory and Items
    Item Database set up and Inventory scripts currently in progress.

---
+++ Minecraft Prototype/Assets/Inventory and Items/Item.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item 
+{
+	public string name;
+	public int id;
+	public string desc;
+	public Texture2D icon;
+	public int power;
+	public int speed;
+	public ItemType itemType;
+		
+		public enum ItemType 
+		{
+		Sword, Shovel, Pickaxe, Block
+		}
+	
+}
